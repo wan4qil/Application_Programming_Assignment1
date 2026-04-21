90f886c58db6c7afd67106fc8bc5de19504525c5
wan4qil <wan4qil@gmail.com>
2026-04-21 11:50:51 +0800

    add UUM Logo and update pubspec.yaml

--- gpa_calculator/lib/main.dart
+++ gpa_calculator/lib/main.dart
@@ -7,27 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Student GPA Calculator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.blueAccent),
       ),
       home: const MyHomePage(title: 'Student GPA Calculator'),
@@ -38,15 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,69 +29,189 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController subject1 = TextEditingController();
+  TextEditingController subject2 = TextEditingController();
+  TextEditingController subject3 = TextEditingController();
+  TextEditingController subject4 = TextEditingController();
 
-  void _incrementCounter() {
+  String gpaResult = '';
+  String statusMessage = '';
+  bool result = false;
+
+  void calculateGPA() {
+    double grade1 = double.tryParse(subject1.text) ?? 0.0;
+    double grade2 = double.tryParse(subject2.text) ?? 0.0;
+    double grade3 = double.tryParse(subject3.text) ?? 0.0;
+    double grade4 = double.tryParse(subject4.text) ?? 0.0;
+
+    double gpa = (grade1 + grade2 + grade3 + grade4) / 4;
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      gpaResult = 'Your GPA is: ${gpa.toStringAsFixed(2)}';
+      statusMessage = gpa >= 2.0 ? 'Status: Passed' : 'Status: Failed';
+      result = true;
     });
+
+  }
+
+  double gradeToPoint(double grade) {
+    if (grade >= 80) return 4.00;
+    if (grade >= 75) return 3.67;
+    if (grade >= 70) return 3.33;
+    if (grade >= 65) return 3.00;
+    if (grade >= 60) return 2.67;
+    if (grade >= 55) return 2.33;
+    if (grade >= 50) return 2.00;
+    if (grade >= 45) return 1.67;
+    if (grade >= 40) return 1.33;
+    if (grade >= 35) return 1.00;
+    return 0.0;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Image.asset(('assets/logo_uum.png'), scale: 2.5,
             ),
+            Text (
+              'Enter your grades for 4 subjects (0-100):',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 20),
+
+            TextField(
+              controller: subject1,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Subject 1',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            TextField(
+              controller: subject2,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Subject 2',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            TextField(
+              controller: subject3,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Subject 3',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            TextField(
+              controller: subject4,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Subject 4',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: calculateGPA,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                    ),
+                    child: Text(
+                      'Calculate GPA',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        subject1.clear();
+                        subject2.clear();
+                        subject3.clear();
+                        subject4.clear();
+                        gpaResult = '';
+                        statusMessage = '';
+                        result = false;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                    ),
+                    child: Text(
+                      'Reset',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )
+                  ),
+                )
+              ],
+            ),
+
+            SizedBox(height: 20),
+
+            if (result)
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: 
+                  BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+
+                child: Column(
+                  children: [
+                    Text(
+                      'Your GPA Result',
+                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      gpaResult,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      statusMessage,
+                      style: TextStyle(fontSize: 16, color: Colors.black54),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              )
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        ),  
+      ),  
     );
   }
 }
